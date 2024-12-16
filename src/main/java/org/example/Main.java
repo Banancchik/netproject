0e86a1a53b36af07d09be89b5ab241fb20a960fd
dani4feed <dan.arbatov@ukr.net>
2024-12-16 20:04:20 +0200

    voting_system 0.3.1 --Host initial setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
-
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Logger;
 
 
-class Vote_view{
+class Vote_view extends Thread{
     private int votes;
 
     Vote_view() {
@@ -28,45 +33,35 @@ class Vote_view{
             return "Insufficient votes for a withdrawal of " + amount + " from the candidate.";
         }
         return "successfully withdrawed " + amount + " votes from the candidate.";
-
     }
 
+    public void run() {
+        System.out.println("hekllo");
+        System.out.println("Current thread: " + Thread.currentThread().getName());
+    }
 }
 
-class vote_choise extends Thread{
 
-
-    public synchronized void run() {
-        try{ Thread.sleep(7);
-        }
-        catch(InterruptedException e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println("Thread name: " + Thread.currentThread().getName());
-        }
-
-}
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Testcomm 2!");
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Server host port was not specified. Please enter the desired port: ");
+        int echoServPort = input.nextInt();
+        input.nextLine();
+        System.out.println("Server started!");
 
-
-        Thread[] threads = new Thread[5];
-
-        System.out.println("--------Executables--------");
-        Vote_view acc = new Vote_view();
-
-        ExecutorService executor = Executors.newCachedThreadPool();
-
-        for (int i = 0; i < 5; i++) {
-            threads[i] = new vote_choise();
-        }
-
-        for (int i = 0; i < 5; i++) {
-            executor.execute(threads[i]);
+        try (ServerSocket servSock = new ServerSocket(echoServPort)) {
+            Logger logger = Logger.getLogger("practical");
+            Executor service = Executors.newCachedThreadPool();
+            while (true) {
+                Socket clientSock = servSock.accept();
+                service.execute(new Vote_view());
+            }
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
         }
     }
-
 }
